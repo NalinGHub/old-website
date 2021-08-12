90c70df70d629bcd2bc90a6e7cfbeb51ee04b4a8
NalinGHub <nalinm01@gmail.com>
2021-08-12 01:25:56 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://nalinghub.github.io/',
+  pathPrefix: '/',
+  title: 'Nalin Mehra',
+  subtitle: 'Personal website',
+  copyright: '© Backpack Technologies 2021',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    // {
+    //   label: 'Articles',
+    //   path: '/'
+    // },
+    {
+      label: 'About',
+      path: '/'
+    },
+    // {
+    //   label: 'Contact me',
+    //   path: '/pages/contacts'
+    // }
+  ],
+  author: {
+    name: 'Nalin Mehra',
+    photo: '/photo.jpg',
+    bio: 'Personal Website!',
+    contacts: {
+      email: 'nalinm01@gmail.com',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'nalinghub',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
---
+++ content/pages/about.md
@@ -0,0 +1,13 @@
+---
+title: "About Me!"
+template: "page"
+date: 09/09/09
+slug : "About"
+---
+
+Hi everyone! I'm Nalin Mehra - an undergraduate student at the [University of Florida](https://www.ufl.edu/) studying Computer Science and economics.
+
+I am currently interning at American Express under the Enterprise Data Technology and Augmented Intelligence organization.
+
+<!-- ![Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.](/media/image-2.jpg) -->
+
